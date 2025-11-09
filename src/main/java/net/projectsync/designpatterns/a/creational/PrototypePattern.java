950cc167741b68ed55377d44d57e1267d0d54233
mohankumarn24 <mohankumar.n24@outlook.com>
2025-11-09 16:24:56 +0530

    add protoype design pattern

---
+++ src/main/java/net/projectsync/designpatterns/a/creational/PrototypePattern.java
@@ -0,0 +1,34 @@
+package net.projectsync.designpatterns.a.creational;
+
+import lombok.Data;
+
+interface Prototype {
+	Prototype clone();
+}
+
+@Data
+class Book implements Prototype {
+	private String data;
+
+	public Book(String data) {
+		this.data = data;
+	}
+
+	@Override
+	public Prototype clone() {
+		return new Book(this.data);
+	}
+}
+
+public class PrototypePattern {
+
+	public static void main(String[] args) {
+
+		Book original = new Book("Original Book");
+		Book copy = (Book) original.clone();
+		copy.setData("Copied Object");
+
+		System.out.println("Original: " + original.getData());
+		System.out.println("Copy: " + copy.getData());
+	}
+}
